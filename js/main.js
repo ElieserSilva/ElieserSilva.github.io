b0c1c10a91f3281fb46055b60262a565c33544b3
ElieserSilva <Novaelirs15@gmail.com>
2021-01-19 11:20:11 -0430

    Primer Commit

---
+++ js/main.js
@@ -0,0 +1,57 @@
+window.addEventListener('DOMContentLoaded',(e)=>{
+    console.log('Start main.js');
+
+    if(localStorage.theme === 'dark'){
+        document.querySelector('html').classList.add('dark');
+        document.querySelector('#DarkModeButton').classList.remove('fa-sun');
+        document.querySelector('#DarkModeButton').classList.add('fa-moon');
+        // document.querySelector('#DarkModeButton').classList.add('text-blue');
+        // document.
+    }
+
+    document.addEventListener('click',e=>{
+        let DomId = e.target.getAttribute('id');
+
+        console.log(DomId);
+
+        switch(DomId){
+            case 'ButtonShowMenu':{
+                document.querySelector('#panelMenuLeft').classList.toggle('hidden');
+                break;
+            }
+
+            case 'DarkModeButton':{
+                e.target.classList.toggle('fa-sun');
+                // e.target.classList.toggle('text-blue');
+                e.target.classList.toggle('fa-moon');
+                // e.target.classList.toggle('text-red');
+                // document.querySelector('#DarkModeButton').classList.add('text-blue');
+
+                let htmlClass = document.querySelector('html').classList;
+
+                if(localStorage.theme == 'dark'){
+                    htmlClass.remove('dark');
+                    localStorage.removeItem('theme');
+                }else{
+                    htmlClass.add('dark');
+                    localStorage.theme = 'dark';
+                }
+
+                break;
+            }
+
+            case 'proyectPTA':{
+                window.open('http://localhost/www.fundaepicaweb.com','_blank');
+                break;
+            }
+
+            case 'proyectGA':{
+                window.open('http://localhost/www.grupoamenca.com','_blank');
+                break;
+            }
+        }
+
+
+    })
+
+})
